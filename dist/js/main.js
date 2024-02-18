8fa1eaee514a08df698f73fe397c96caa3ff499e
Anastasiia <your_email@whatever.com>
2024-02-18 20:56:35 +0200

    ADD products_swiper responsiveness

--- dist/js/main.js
+++ dist/js/main.js
@@ -8,11 +8,19 @@ const swiper = new Swiper(".testimonials__swiper", {
 });
 const products_swiper = new Swiper(".products__swiper", {
   // Optional parameters
-  slidesPerView: 3,
+  slidesPerView: 1,
   spaceBetween: 20,
   pagination: {
     el: ".products-swiper-pagination",
     clickable: true,
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+    },
+    1220: {
+      slidesPerView: 3,
+    },
   },
 });
 
